ae4ed050a85d2200a6d3d7a6ee728d07e7ddad58
craftwerk <pst@crftwrk.de>
2021-12-26 11:16:16 +0100

    Set bs_cookie as default, cleanup init.js

--- js/cookieconsent.js
+++ js/cookieconsent.js
@@ -23,7 +23,7 @@
             'current_lang': 'en',
             'auto_language': null,
             'autorun': true,                          // run as soon as loaded
-            'cookie_name': 'cc_cookie',
+            'cookie_name': 'bs_cookie',
             'cookie_expiration': 182,                 // default: 6 months (in days)
             'cookie_domain': window.location.hostname,       // default: current domain
             'cookie_path': '/',
@@ -2075,144 +2075,99 @@
 
 
 
-
 // Init
-window.addEventListener('load', function(){
-            
-            // obtain plugin
-            var cc = initCookieConsent();
+window.addEventListener('load', function () {
 
-            // run plugin with your configuration
-            cc.run({
-                current_lang: 'en',
-                autoclear_cookies: true,                   // default: false
-                //theme_css: 'css/cookieconsent.css',  // replace with a valid path
-                page_scripts: true,                        // default: false
+  // obtain plugin
+  var cc = initCookieConsent();
 
-                // delay: 0,                               // default: 0
-                // auto_language: '',                      // default: null; could also be 'browser' or 'document'
-                // autorun: true,                          // default: true
-                // force_consent: false,                   // default: false
-                // hide_from_bots: false,                  // default: false
-                // remove_cookie_tables: false             // default: false
-                 cookie_name: 'bs_cookie',               // default: 'cc_cookie'
-                // cookie_expiration: 182,                 // default: 182 (days)
-                // cookie_domain: location.hostname,       // default: current domain
-                // cookie_path: '/',                       // default: root
-                // cookie_same_site: 'Lax',                // default: 'Lax'
-                // use_rfc_cookie: false,                  // default: false
-                // revision: 0,                            // default: 0
+  // run plugin with your configuration
+  cc.run({
+    current_lang: 'en',
+    autoclear_cookies: true,                   // default: false
+    page_scripts: true,                        // default: false
 
-              /*
-                onAccept: function (cookie) {
-                    // ...
+    languages: {
+      'en': {
+        consent_modal: {
+          title: 'We use cookies!',
+          description: 'We use cookies on our website to give you the most relevant experience by remembering your preferences and repeat visits. By clicking “Accept all”, you consent to the use of all the cookies. However, you may visit "Cookie settings" to provide a controlled consent. <a href="javascript:void(0)" data-cc="c-settings" class="cc-link btn-link">Cookie settings</a>',
+          primary_btn: {
+            text: 'Accept all',
+            role: 'accept_all'              // 'accept_selected' or 'accept_all'
+          },
+          secondary_btn: {
+            text: 'Reject all',
+            role: 'accept_necessary'        // 'settings' or 'accept_necessary'
+          }
+        },
+
+        settings_modal: {
+          title: 'Cookie preferences',
+          save_settings_btn: 'Save settings',
+          accept_all_btn: 'Accept all',
+          reject_all_btn: 'Reject all',
+          close_btn_label: 'Close',
+          cookie_table_headers: [
+            { col1: 'Name' },
+            { col2: 'Domain' },
+            { col3: 'Expiration' },
+            { col4: 'Description' }
+          ],
+          blocks: [
+            {
+              title: 'Cookie usage',
+              description: 'I use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full <a href="#" class="cc-link">privacy policy</a>.'
+            }, {
+              title: 'Necessary',
+              description: 'These cookies are essential for the proper functioning of my website. Without these cookies, the website would not work properly',
+              toggle: {
+                value: 'necessary',
+                enabled: true,
+                readonly: true          // cookie categories with readonly=true are all treated as "necessary cookies"
+              }
+            }, {
+              title: 'Analytics',
+              description: 'These cookies allow the website to remember the choices you have made in the past',
+              toggle: {
+                value: 'analytics',     // your cookie category
+                enabled: false,
+                readonly: false
+              },
+              cookie_table: [             // list of all expected cookies
+                {
+                  col1: '^_ga',       // match all cookies starting with "_ga"
+                  col2: 'google.com',
+                  col3: '2 years',
+                  col4: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+                  is_regex: true
                 },
-
-                onChange: function (cookie, changed_preferences) {
-                    // ...
-                },
-                */
-              
-              /*
-                gui_options: {
-                  consent_modal: {
-                    //layout: 'cloud',               // box/cloud/bar
-                    //position: 'bottom center',     // bottom/middle/top + left/right/center
-                    //transition: 'slide'            // zoom/slide
-                  },
-                    settings_modal: {
-                      //layout: 'box',                 // box/bar
-                      // position: 'left',           // left/right
-                      //transition: 'slide'            // zoom/slide
-                    }
-                },
-                */
-
-                languages: {
-                    'en': {
-                        consent_modal: {
-                            title: 'We use cookies!',
-                            description: 'We use cookies on our website to give you the most relevant experience by remembering your preferences and repeat visits. By clicking “Accept all”, you consent to the use of all the cookies. However, you may visit "Cookie settings" to provide a controlled consent. <a href="javascript:void(0)" data-cc="c-settings" class="cc-link btn-link">Cookie settings</a>',
-                            primary_btn: {
-                                text: 'Accept all',
-                                role: 'accept_all'              // 'accept_selected' or 'accept_all'
-                            },
-                            secondary_btn: {
-                                text: 'Reject all',
-                                role: 'accept_necessary'        // 'settings' or 'accept_necessary'
-                            }
-                          },
-
-                        settings_modal: {
-                            title: 'Cookie preferences',
-                            save_settings_btn: 'Save settings',
-                            accept_all_btn: 'Accept all',
-                            reject_all_btn: 'Reject all',
-                            close_btn_label: 'Close',
-                            cookie_table_headers: [
-                                {col1: 'Name'},
-                                {col2: 'Domain'},
-                                {col3: 'Expiration'},
-                                {col4: 'Description'}
-                            ],
-                            blocks: [
-                                {
-                                    title: 'Cookie usage',
-                                    description: 'I use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full <a href="#" class="cc-link">privacy policy</a>.'
-                                }, {
-                                    title: 'Necessary',
-                                    description: 'These cookies are essential for the proper functioning of my website. Without these cookies, the website would not work properly',
-                                    toggle: {
-                                        value: 'necessary',
-                                        enabled: true,
-                                        readonly: true          // cookie categories with readonly=true are all treated as "necessary cookies"
-                                    }
-                                }, {
-                                    title: 'Analytics',
-                                    description: 'These cookies allow the website to remember the choices you have made in the past',
-                                    toggle: {
-                                        value: 'analytics',     // your cookie category
-                                        enabled: false,
-                                        readonly: false
-                                    },
-                                    cookie_table: [             // list of all expected cookies
-                                        {
-                                            col1: '^_ga',       // match all cookies starting with "_ga"
-                                            col2: 'google.com',
-                                            col3: '2 years',
-                                            col4: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-                                            is_regex: true
-                                        },
-                                        {
-                                            col1: '_gid',
-                                            col2: 'google.com',
-                                            col3: '1 day',
-                                            col4: 'description ...',
-                                        }
-                                    ]
-                                }, {
-                                    title: 'Advertisement',
-                                    description: 'These cookies collect information about how you use the website, which pages you visited and which links you clicked on. All of the data is anonymized and cannot be used to identify you',
-                                    toggle: {
-                                        value: 'targeting',
-                                        enabled: false,
-                                        readonly: false
-                                    }
-                                }, {
-                                    title: 'More information',
-                                    description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="#yourcontactpage">contact us</a>.',
-                                }, 
-                              
-                            ]
-                        }
-                      
-                    }
+                {
+                  col1: '_gid',
+                  col2: 'google.com',
+                  col3: '1 day',
+                  col4: 'description ...',
                 }
-              
-              
-              
-            });
-        });
-        
-        
+              ]
+            }, {
+              title: 'Advertisement',
+              description: 'These cookies collect information about how you use the website, which pages you visited and which links you clicked on. All of the data is anonymized and cannot be used to identify you',
+              toggle: {
+                value: 'marketing',
+                enabled: false,
+                readonly: false
+              }
+            }, {
+              title: 'More information',
+              description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="#yourcontactpage">contact us</a>.',
+            },
+
+          ]
+        }
+
+      }
+    }
+
+  });
+});
 

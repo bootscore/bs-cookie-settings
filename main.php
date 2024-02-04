aadfd92c3eddd541842015c947ffb9b27e1a1af3
craftwerk <pst@crftwrk.de>
2024-02-04 17:27:36 +0100

    Move css and js to assets folder

--- main.php
+++ main.php
@@ -37,9 +37,9 @@ $myUpdateChecker->setBranch('main');
  */
 function bs_cookie_settings() {
     
-  wp_enqueue_script( 'cookie-settings-js', plugins_url( '/js/cookie-settings.min.js' , __FILE__ ), array(), false, true );
+  wp_enqueue_script( 'cookie-settings-js', plugins_url( '/assets/js/cookie-settings.min.js' , __FILE__ ), array(), false, true );
     
-  wp_register_style( 'cookie-settings-css', plugins_url('/css/cookie-settings.min.css', __FILE__) );
+  wp_register_style( 'cookie-settings-css', plugins_url('/assets/css/cookie-settings.min.css', __FILE__) );
   wp_enqueue_style( 'cookie-settings-css' );
     
 }
